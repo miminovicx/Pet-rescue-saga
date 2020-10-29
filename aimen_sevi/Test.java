f68e4a27bde648494d2a147643613b928f004b14
Sevi Dervishi <sevidervishi@outlook.fr>
2020-10-29 16:58:25 +0100

    coding player class

--- aimen_sevi/Test.java
+++ aimen_sevi/Test.java
@@ -1,9 +1,49 @@
-import levelpack.Level;
+import levelpack.*;
+import levelpack.FieldElement;
+import pack.Player;
+// import levelpack.Level;
+// import levelpack.Field;
+// import levelpack.FieldElements;
 public class Test
 {
+  public static void squaredString(String str)
+  {
+    String line = "=";
+    System.out.print("*" + line.repeat(str.length()) + "*\n");
+    System.out.print("*" + str + "*\n");
+    System.out.print("*" + line.repeat(str.length()) + "*\n");
+  }
   public static void main(String[] args)
   {
+    squaredString("LevelTest");
     Level lvl = new Level(0,1,2,0,false);
-  System.out.println(lvl);
+    System.out.println(lvl);
+    System.out.println("-------------------------------------------------------");
+    System.out.println();
+
+    squaredString("FieldElement Test");
+    FieldElement element = new FieldElement(0,0);
+    FieldElement element1 = new FieldElement(0,1);
+    // FieldElement element2 = new FieldElement(1,0);
+    // FieldElement element3 = new FieldElement(1,1);
+    System.out.println(element);
+    System.out.println("-------------------------------------------------------");
+    System.out.println();
+
+    squaredString("Field Test");
+    Field field = new Field(2,2);
+    field.putElement(element);
+    field.putElement(element1);
+    //field.putElement(element2);
+    //field.putElement(element3);
+    System.out.print(field);
+    System.out.println("-------------------------------------------------------");
+    System.out.println();
+
+    squaredString("Player Test");
+    Player player = new Player("Sevi", 5, 100);
+    System.out.print(player);
+    System.out.println("-------------------------------------------------------");
+    System.out.println();
   }
 }
