620332e0c5a3618860e6d4dfea1d8e9e46fbfacc
miminovicx <xxmimiovicx@gmail.com>
2020-10-29 11:47:07 +0100

    level class update + launcher

--- aimen_sevi/Test.java
+++ aimen_sevi/Test.java
@@ -3,7 +3,7 @@ public class Test
 {
   public static void main(String[] args)
   {
-    Level lvl = new Level(0,1,2);
+    Level lvl = new Level(0,1,2,0,false);
   System.out.println(lvl);
   }
 }
--- aimen_sevi/levelpack/Level.java
+++ aimen_sevi/levelpack/Level.java
@@ -5,25 +5,26 @@ private int num;
 private int stars;
 private int score;
 private int lastScore;
-private Field field;
-private boolean succeded;
-private int[] palier;
-  public Level(int num,int stars,int score,int lastScore,Field field,boolean succeded,int[] palier)
+//private Field field;
+private boolean succeded=true;
+//private int[] palier;
+  public Level(int num,int stars,int score,int lastScore/*,Field field*/,boolean succeded/*, int[] palier*/)
   {
     this.num = num;
     this.stars = stars;
     this.score = score;
     this.lastScore = lastScore;
-    this.field = field;
+    //this.field = field;
     this.succeded = succeded;
-    this.palier = palier;
+    //this.palier = palier;
   }
   public String toString()
   {
     if(succeded)
     {
-    return("Level : "+this.num+"\nStars :"+this.stars+"\nScore : "+this.score+"\n");
+    return("Level : "+this.num+"\nStars :"+this.stars+"\nObjectif : "+this.score+"\nLast score : "+this.lastScore);
     }
+    return ("Level : "+this.num+"\nStars :"+this.stars+"\nObjectif : "+this.score);
   }
 
 }
