24673f5d5ff269e86fe0cb2d7e4382f26e558b07
Sevi Dervishi <sevidervishi@outlook.fr>
2020-11-16 14:19:34 +0100

    coding WoodBlock and BonusBlock classes

---
+++ aimen_sevi/levelpack/WoodBlock.java
@@ -0,0 +1,25 @@
+package aimen_sevi.levelpack;
+/**
+ * Cette classe représente un bloc de bois (imobile et impossible à supprimmer)
+ */
+class WoodBlock extends FieldElement
+{
+  /**
+   * Constructeur d'un WoodBlock
+   * @method WoodBlock
+   * @param  x         abscisse
+   * @param  y         ordonée
+   */
+  public WoodBlock(int x, int y)
+  {
+    super(x, y);
+  }
+  /**
+   * Cette méthode permet d'afficher un WoodBlock
+   * @method toString
+   * @return chaîne qui sera affiché
+   */
+  public String toString()
+  {
+      return ("\\");
+  }
